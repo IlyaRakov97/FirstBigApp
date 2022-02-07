fdd92ec7dff67773edc8e84ff715b4a9e118a8c0
IlyaRakov97 <balonka97@mail.ru>
2022-02-07 10:06:37 +0400

    Controllers

---
+++ FirstBigApp/Controllers/ScheduleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleViewController.swift
+//  FirstBigApp
+//
+//  Created by Илья Раков on 07.02.2022.
+//
+
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    title = "Schedule"
+    }
+}
---
+++ FirstBigApp/Controllers/TasksViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TasksViewController.swift
+//  FirstBigApp
+//
+//  Created by Илья Раков on 07.02.2022.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title  = "Task"
+        
+    }
+}
