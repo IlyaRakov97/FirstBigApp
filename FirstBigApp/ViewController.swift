1f517773546e1cb25f2929318fe4e0b240ac3040
IlyaRakov97 <balonka97@mail.ru>
2022-02-07 09:06:50 +0400

    Initial Commit

---
+++ FirstBigApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstBigApp
+//
+//  Created by Илья Раков on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
