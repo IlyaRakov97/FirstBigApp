02194becffc5c9ef8092b6685c95ef98b004f35f
IlyaRakov97 <balonka97@mail.ru>
2022-02-07 14:14:46 +0400

    showHideButton

--- FirstBigApp/Controllers/ScheduleViewController.swift
+++ FirstBigApp/Controllers/ScheduleViewController.swift
@@ -7,12 +7,83 @@
 
 
 import UIKit
+import FSCalendar
 
 class ScheduleViewController: UIViewController {
+    
+    var calendarHeightConstraint: NSLayoutConstraint!
+    
+    private var calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        
+        return calendar
+                
+    }()
+    
+    let showHideButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Open calendar", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        
+        
+        
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-    title = "Schedule"
+        title = "Schedule"
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        
+        setConstraints()
+        
     }
+}
+
+
+//MARK: - FSCalendarDataSource, FSCalendarDelegate
+
+
+extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegate{
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+}
+
+
+//MARK: - SetConstraints
+
+extension ScheduleViewController{
+    
+    func setConstraints() {
+        view.addSubview(calendar)
+        
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint)
+        
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            
+        ])
+        view.addSubview(showHideButton)
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            showHideButton.widthAnchor.constraint(equalToConstant: 100),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
 }
