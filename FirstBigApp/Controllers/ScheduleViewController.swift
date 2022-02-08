fc43b42b451245e036c74d061d959aaf52f3c83a
IlyaRakov97 <balonka97@mail.ru>
2022-02-08 10:30:27 +0400

    Swipe

--- FirstBigApp/Controllers/ScheduleViewController.swift
+++ FirstBigApp/Controllers/ScheduleViewController.swift
@@ -22,6 +22,7 @@ class ScheduleViewController: UIViewController {
     }()
     
     let showHideButton: UIButton = {
+        
         let button = UIButton()
         button.setTitle("Open calendar", for: .normal)
         button.setTitleColor(.black, for: .normal)
@@ -43,10 +44,51 @@ class ScheduleViewController: UIViewController {
         calendar.delegate = self
         calendar.dataSource = self
         
+        calendar.scope = .week
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
         
         setConstraints()
+        swipeAction()
         
     }
+    
+    @objc func showHideButtonTapped(){
+        
+        if calendar.scope == .week{
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Close calendar", for: .normal)
+        }else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Open calendar", for: .normal)
+            
+        }
+        
+    }
+    
+    //MARK: - SwipeGestorRecognizer
+    
+    func swipeAction (){
+        let swipeUP = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUP.direction = .up
+        calendar.addGestureRecognizer(swipeUP)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer){
+        switch gesture.direction {
+        case .up:
+            showHideButtonTapped()
+        case .down:
+            showHideButtonTapped()
+        default:
+            break
+        }
+    }
+    
 }
 
 
@@ -54,9 +96,13 @@ class ScheduleViewController: UIViewController {
 
 
 extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegate{
+    
     func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
         calendarHeightConstraint.constant = bounds.height
         view.layoutIfNeeded()
+    }
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
     }
 }
 
